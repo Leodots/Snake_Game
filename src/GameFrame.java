1adfd39ccac199a9d28a466cc44dd9998c6dad32
leodots <2212.leonardo@gmail.com>
2022-03-03 16:51:16 -0300

    Fixing title name

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,11 +1,11 @@
 import javax.swing.*;
 
-public class GameFrame extends JFrame {
+public class  GameFrame extends JFrame {
 
     public GameFrame(){
 
         this.add(new GamePanel());
-        this.setTitle("Snake");
+        this.setTitle("Snake Game");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
         this.pack();
